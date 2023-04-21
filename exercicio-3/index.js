f9ae986ac29c49f545b4a68e2e91e781747d7b5e
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-04-21 08:08:49 -0300

    criando imagem no DOM

--- exercicio-3/index.js
+++ exercicio-3/index.js
@@ -16,3 +16,13 @@ const input = new Input(form, { id: 'name' });
 
 form.addChildren(label, input);
 form.render();
+
+const image = new Img(
+  'https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/12/gollum-o-senhor-dos-aneis.webp',
+  'body',
+  { width: '300', height: '300' },
+);
+image.render();
+image.changeImg(
+  'https://classic.exame.com/wp-content/uploads/2016/09/size_960_16_9_20151019-14270-1x1de9d.png?w=960',
+);
